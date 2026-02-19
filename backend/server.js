8c3b6b6bd18da249587d89b47c2a1c101a186ce9
Murillo Ramos <murillo.tech.dev@gmail.com>
2026-02-19 20:41:41 -0300

    Setting up backend/server logic

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,17 @@ import "dotenv/config";
 
 const app = express();
 const API_KEY = process.env.ETHERSCAN_API_KEY;
+const PORT = 3000;
 
 app.get("/latest-block", async (req, res) => {
-    const url = `https:https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=${API_KEY}`;
+    const url = `https://api.etherscan.io/v2/api?chainid=1&module=proxy&action=eth_blockNumber&apikey=${API_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
     res.json(data);
 });
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
 
 
