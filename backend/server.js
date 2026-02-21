63e39559e8069651a8fdf5d69d2f780443e89469
Murillo Ramos <murillo.tech.dev@gmail.com>
2026-02-21 12:54:34 -0300

    Linking frontend with backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import fetch from "node-fetch";
+import path from "path"
+import { fileURLToPath } from "url";
 import "dotenv/config";
 
 const app = express();
 const API_KEY = process.env.ETHERSCAN_API_KEY;
 const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/latest-block", async (req, res) => {
     const url = `https://api.etherscan.io/v2/api?chainid=1&module=proxy&action=eth_blockNumber&apikey=${API_KEY}`;
@@ -17,4 +24,8 @@ app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
 
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    next();
+})
